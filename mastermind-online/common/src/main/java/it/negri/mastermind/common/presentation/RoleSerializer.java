fa73eaee0487162e8539741c9eab6368742e72f1
Andrea Negri <andrea.negri4@studio.unibo.it>
2023-05-17 15:44:04 +0200

    Create classi per la serializzazione/deserializzazione del modello

---
+++ mastermind-online/common/src/main/java/it/negri/mastermind/common/presentation/RoleSerializer.java
@@ -0,0 +1,4 @@
+package it.negri.mastermind.common.presentation;
+
+public class RoleSerializer {
+}
--- mastermind-online/common/src/main/java/it/negri/mastermind/common/utils/GsonUtils.java
+++ mastermind-online/common/src/main/java/it/negri/mastermind/common/utils/GsonUtils.java
@@ -2,6 +2,8 @@ package it.negri.mastermind.common.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import it.negri.mastermind.common.model.*;
+import it.negri.mastermind.common.presentation.*;
 
 public class GsonUtils {
 
@@ -9,6 +11,16 @@ public class GsonUtils {
         return new GsonBuilder()
                 .excludeFieldsWithoutExposeAnnotation()
                 .serializeNulls()
+                .registerTypeAdapter(Game.class, new GameSerializer())
+                .registerTypeAdapter(Game.class, new GameDeserializer())
+                .registerTypeAdapter(Lobby.class, new LobbySerializer())
+                .registerTypeAdapter(Lobby.class, new LobbyDeserializer())
+                .registerTypeAdapter(Player.class, new PlayerSerializer())
+                .registerTypeAdapter(Player.class, new PlayerDeserializer())
+                .registerTypeAdapter(ResultLabel.class, new ResultLabelSerializer())
+                .registerTypeAdapter(ResultLabel.class, new ResultLabelDeserializer())
+                .registerTypeAdapter(Role.class, new RoleSerializer())
+                .registerTypeAdapter(Role.class, new RoleDeserializer())
                 .create();
     }
 
