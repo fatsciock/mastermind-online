67030ae14b8d877a2855a3b05c3b29d07e5521af
Andrea Negri <andrea.negri4@studio.unibo.it>
2023-05-19 15:03:12 +0200

    Implementato MastermandService (documentazione Swagger consultabile)

--- mastermind-online/server/src/main/java/it/negri/mastermind/server/MastermindService.java
+++ mastermind-online/server/src/main/java/it/negri/mastermind/server/MastermindService.java
@@ -1,21 +1,47 @@
 package it.negri.mastermind.server;
 
+import io.javalin.Javalin;
+import it.negri.mastermind.common.LocalMastermind;
+import it.negri.mastermind.common.Mastermind;
+import it.negri.mastermind.common.utils.GsonUtils;
+import it.negri.mastermind.server.games.GameController;
+import it.negri.mastermind.server.lobbies.LobbyController;
+import it.negri.mastermind.server.players.PlayerController;
+import it.negri.mastermind.server.utils.Filters;
+import it.negri.mastermind.server.utils.JavalinGsonAdapter;
+import it.negri.mastermind.server.utils.Plugins;
+
 public class MastermindService {
 
     private static final String API_VERSION = "0.1.0";
-
     public static final String BASE_URL = "/mastermind/v" + API_VERSION;
+    private static final int DEFAULT_PORT = 10000;
+    private final int port;
+    private final Javalin server;
 
     public MastermindService(int port) {
+        this.port = port;
+        server = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+            config.jsonMapper(new JavalinGsonAdapter(GsonUtils.createGson()));
+            config.plugins.register(Plugins.openApiPlugin(API_VERSION, "/doc"));
+            config.plugins.register(Plugins.swaggerPlugin("/doc", "/ui"));
+            config.plugins.register(Plugins.routeOverviewPlugin("/routes"));
+        });
 
+        server.before(path("/*"), Filters.putSingletonInContext(Mastermind.class, new LocalMastermind()));
+
+        GameController.of(path("/games")).registerRoutes(server);
+        LobbyController.of(path("/lobbies")).registerRoutes(server);
+        PlayerController.of(path("/players")).registerRoutes(server);
     }
 
     public void start() {
-
+        server.start(port);
     }
 
     public void stop() {
-
+        server.stop();
     }
 
     private static String path(String subPath) {
@@ -23,6 +49,6 @@ public class MastermindService {
     }
 
     public static void main(String[] args) {
-
+        new MastermindService(args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_PORT).start();
     }
 }
--- mastermind-online/server/src/main/java/it/negri/mastermind/server/utils/JavalinGsonAdapter.java
+++ mastermind-online/server/src/main/java/it/negri/mastermind/server/utils/JavalinGsonAdapter.java
@@ -1,9 +1,10 @@
-package it.negri.mastermind.server;
+package it.negri.mastermind.server.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 import io.javalin.http.BadRequestResponse;
 import io.javalin.json.JsonMapper;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
 import java.util.Objects;
@@ -16,13 +17,15 @@ public class JavalinGsonAdapter implements JsonMapper {
         this.gson = Objects.requireNonNull(gson);
     }
 
+    @NotNull
     @Override
-    public String toJsonString(Object obj, Type type) {
+    public String toJsonString(@NotNull Object obj, @NotNull Type type) {
         return gson.toJson(obj, type);
     }
 
+    @NotNull
     @Override
-    public <T> T fromJsonString(String json, Type targetType) {
+    public <T> T fromJsonString(@NotNull String json, @NotNull Type targetType) {
         try {
             return gson.fromJson(json, targetType);
         } catch (JsonParseException e) {
